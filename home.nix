76c03bff9774ec0e160867368b9854e8a4ae342e
Moritz Heiber <hello@heiber.im>
2024-03-09 20:22:24 +0100

    Added mcfly, starship, variables, disabled news

--- home.nix
+++ home.nix
@@ -4,18 +4,40 @@
   home.username = "mheiber";
   home.homeDirectory = "/home/mheiber";
   home.stateVersion = "23.11";
+  news.display = "silent";
 
   home.packages = [
+    pkgs.nixpkgs-fmt
   ];
 
-  home.file = {
-  };
+  home.file = { };
 
   home.sessionVariables = {
+    DOCKER_BUILDKIT = 1;
+    GTK_IM_MODULE = "xim";
+    EDITOR = "vim";
+    BROWSER = "firefox";
   };
 
-  programs.home-manager.enable = true;
-  programs.bash = {
-    enable = true;
+  programs = {
+    home-manager = {
+      enable = true;
+    };
+    bash = {
+      enable = true;
+    };
+    starship = {
+      enable = true;
+      enableBashIntegration = true;
+    };
+    mcfly = {
+      enable = true;
+      enableBashIntegration = true;
+      fzf.enable = true;
+    };
+    fzf = {
+      enable = true;
+      enableBashIntegration = true;
+    };
   };
 }
